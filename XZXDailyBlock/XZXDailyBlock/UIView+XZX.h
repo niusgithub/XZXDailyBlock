4645c41bf30b616f7415b31b52a1edbf58e7ebbd
niusgithub <niusgithub@users.noreply.github.com>
2016-07-16 23:58:46 +0800

    添加一个category

---
+++ XZXDailyBlock/XZXDailyBlock/UIView+XZX.h
@@ -0,0 +1,21 @@
+//
+//  UIView+XZX.h
+//  XAlbum
+//
+//  Created by yunmu on 15/7/7.
+//  Copyright (c) 2015年 yunmu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (XZX)
+
+@property (nonatomic, assign) CGFloat x;
+@property (nonatomic, assign) CGFloat y;
+@property (nonatomic, assign) CGFloat centerX;
+@property (nonatomic, assign) CGFloat centerY;
+@property (nonatomic, assign) CGFloat width;
+@property (nonatomic, assign) CGFloat height;
+@property (nonatomic, assign) CGSize size;
+
+@end
